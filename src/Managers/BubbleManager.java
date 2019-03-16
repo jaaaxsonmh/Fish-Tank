6d30cf3c7f75915b4f93d0f7f4de43249c6ffe9c
Jack hosking <jackhosking2@outlook.com>
2019-03-17 01:32:47 +1300

    Bubbles turn on and off and remove size using an extra collection

--- src/Managers/BubbleManager.java
+++ src/Managers/BubbleManager.java
@@ -10,16 +10,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BubbleManager {
-    private final static Colour WHITE = new Colour( 1.0f, 1.0f, 1.0f);
+    private final static Colour WHITE = new Colour(1.0f, 1.0f, 1.0f);
 
     private List<Bubble> bubbles = new ArrayList<>();
+    private List<Bubble> toRemove = new ArrayList<>();
     private static final int BUBBLE_AMOUNT = 40;
     public boolean enabled;
     float transparency, radius, offsetX, offsetY, age;
 
     public void populate() {
 
-        if(BUBBLE_AMOUNT > bubbles.size()) {
+        if (BUBBLE_AMOUNT > bubbles.size()) {
             transparency = Rand.getFloatBetween(0.5f, 1.0f);
             radius = Rand.getFloatBetween(0.03f, 0.05f);
             offsetX = Rand.getFloatBetween(0.7f, 0.9f);
@@ -32,34 +33,37 @@ public class BubbleManager {
     }
 
     public void draw(GL2 gl) {
-        if(enabled) {
+        if (enabled) {
             populate();
             for (Bubble bub : bubbles) {
                 bub.draw(gl);
                 bub.animate(gl);
                 reset();
             }
-        } else if (!enabled) {
+        } else {
             for (Bubble bub : bubbles) {
                 bub.draw(gl);
                 bub.animate(gl);
-                remove();
+
             }
+            hardReset();
+            bubbles.removeAll(toRemove);
+            System.out.println(bubbles.size());
         }
     }
 
 
     public void reset() {
-        for(Bubble bub : bubbles) {
+        for (Bubble bub : bubbles) {
             float resetY = Rand.getFloatBetween(-0.7f, -0.8f);
             float resetX = Rand.getFloatBetween(0.7f, 0.9f);
             float radius = Rand.getFloatBetween(0.03f, 0.05f);
             float transparency = Rand.getFloatBetween(0.5f, 1.0f);
 
-            if(bub.offsetY >= Water.WAVE_WATER_HEIGHT) {
+            if (bub.offsetY >= Water.WAVE_WATER_HEIGHT) {
                 bub.offsetY = resetY;
             }
-            if(bub.transparency < 0.0f || bub.radius < 0.0f) {
+            if (bub.transparency < 0.0f || bub.radius < 0.0f) {
                 bub.offsetY = resetY;
                 bub.offsetX = resetX;
                 bub.transparency = transparency;
@@ -68,15 +72,13 @@ public class BubbleManager {
         }
     }
 
-    public void remove() {
-        if(!enabled) {
-            for(Bubble bub : bubbles) {
-                if(bub.offsetY >= Water.WAVE_WATER_HEIGHT) {
-                    bubbles.remove(bub);
-                }
-                if(bub.transparency < 0.0f || bub.radius < 0.0f) {
-                    bubbles.remove(bub);
-                }
+    public void hardReset() {
+        for (Bubble bub : bubbles) {
+            if (bub.offsetY >= Water.WAVE_WATER_HEIGHT) {
+                toRemove.add(bub);
+            }
+            if (bub.transparency < 0.0f || bub.radius < 0.0f) {
+               toRemove.add(bub);
             }
         }
     }
